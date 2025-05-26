a4e0538f8a43a88494853f5e4217c6c94e7059e5
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-26 16:10:25 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 # Placeholder for storing API tokens
-SHOPIFY_API_KEY = "your-shopify-token"
-AUTODS_API_KEY = "your-autods-token"
+SHOPIFY_API_KEY = "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"
+AUTODS_API_KEY = "d9d7854c-db93-4c39-96ce-be0b1a3be7db"
