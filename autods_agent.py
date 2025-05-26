bc1074558dc9269f8428637a5e18ddb806fdaaa3
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-26 16:14:22 -0400

    Update autods_agent.py

--- autods_agent.py
+++ autods_agent.py
@@ -1,6 +1,6 @@
 import requests
 
-AUTODS_API_KEY = 'your_autods_api_key'
+AUTODS_API_KEY = 'd9d7854c-db93-4c39-96ce-be0b1a3be7db'
 
 def fetch_and_push_products():
     # Placeholder for real AutoDS logic
