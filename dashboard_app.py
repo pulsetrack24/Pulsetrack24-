5c9faf684008d77987448a0a71bd453637477abb
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-27 13:58:40 -0400

    Delete dashboard_app.py

--- dashboard_app.py
+++
@@ -1,6 +0,0 @@
-def create_dashboard(products):
-    html = "<h1>ProfitBot v6.4 Dashboard</h1><table border='1'><tr><th>Title</th><th>Price</th></tr>"
-    for p in products:
-        html += f"<tr><td>{p['title']}</td><td>{p['price']}</td></tr>"
-    html += "</table>"
-    return html
