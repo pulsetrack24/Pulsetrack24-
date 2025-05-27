aa9e3d0ab1f635958ee1003567390d70f0b3905f
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-27 17:35:08 -0400

    Delete ai_optimizer.py

--- ai_optimizer.py
+++
@@ -1,9 +0,0 @@
-def optimize_products(products):
-    final = []
-    for p in products:
-        try:
-            if (p["price"] - p["cost"]) / p["cost"] > 0.5:
-                final.append(p)
-        except:
-            continue
-    return final
