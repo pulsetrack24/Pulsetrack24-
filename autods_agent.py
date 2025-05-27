f40ead9d37bd417ed118820f75d1b9318bf3cea0
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-27 12:49:26 -0400

    Delete autods_agent.py

--- autods_agent.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-AUTODS_API_KEY = os.getenv("d9d7854c-db93-4c39-96ce-be0b1a3be7db")
-
-def fetch_and_push_products():
-    print("Simulating AutoDS fetch and push...")
-    # Add real logic here to use AutoDS API
