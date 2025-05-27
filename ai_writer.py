416a1ac051e184120b88f59da663324c75c6b403
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-27 13:58:16 -0400

    Delete ai_writer.py

--- ai_writer.py
+++
@@ -1,6 +0,0 @@
-def generate_product_data():
-    return [{
-        "title": "Herbal Detox Tea",
-        "description": "Boost your health with our premium detox tea, packed with antioxidants.",
-        "price": "19.99"
-    }]
