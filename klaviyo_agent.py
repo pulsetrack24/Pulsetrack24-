6bacf76284b280a5eab93606e405357a7c9f67cd
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-27 16:29:23 -0400

    Delete klaviyo_agent.py

--- klaviyo_agent.py
+++
@@ -1,5 +0,0 @@
-import requests
-from settings import KLAVIYO_API_KEY
-
-def send_klaviyo_campaign(products):
-    return [{"subject": f"New: {p['title']}", "body": p['description']} for p in products]
