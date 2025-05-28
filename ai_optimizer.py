0fcf793e8308a5943324dca0f85547b5830a5304
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-28 01:24:05 -0400

    Delete ai_optimizer.py

--- ai_optimizer.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-import os
-
-client = OpenAI(api_key="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")
-
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": prompt}],
-)
