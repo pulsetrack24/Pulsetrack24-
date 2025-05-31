f387534556d84dbdfc8e0398f3a45f2f5862e142
pulsetrack24 <pulsetrack247@gmail.com>
2025-05-30 21:37:29 -0400

    Update shopify_connector.py

--- shopify_connector.py
+++ shopify_connector.py
@@ -1,11 +1,10 @@
-# shopify_connector.py
 import os
 import requests
 
 SHOPIFY_STORE_NAME = os.getenv("SHOPIFY_STORE_NAME")
 SHOPIFY_ACCESS_TOKEN = os.getenv("SHOPIFY_ACCESS_TOKEN")
 
-BASE_URL = f"https://{SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2023-07"
+BASE_URL = f"https://{SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2023-10"
 
 HEADERS = {
     "Content-Type": "application/json",
